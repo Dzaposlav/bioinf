6e7ef1be6bc97ef5137352aff618907095c494cd
ledasko <MedicJePeder123#>
2018-01-16 15:10:56 +0100

    added wavelet

---
+++ src/wavelet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ledasko on 16-Jan-18.
+//
+
+#include "wavelet.h"
---
+++ src/wavelet.h
@@ -0,0 +1,14 @@
+//
+// Created by ledasko on 16-Jan-18.
+//
+
+#ifndef BIOINF_WAVELET_H
+#define BIOINF_WAVELET_H
+
+
+class wavelet {
+
+};
+
+
+#endif //BIOINF_WAVELET_H
